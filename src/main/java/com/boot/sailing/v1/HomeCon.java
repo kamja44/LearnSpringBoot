77adb1c59e531cf4889d8fbfdcb0d8826e878f94
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-02-26 16:39:25 +0900

    RequestMapping, GetMapping, PostMapping

--- src/main/java/com/boot/sailing/v1/HomeCon.java
+++ src/main/java/com/boot/sailing/v1/HomeCon.java
@@ -1,13 +1,23 @@
 package com.boot.sailing.v1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeCon {
-
-    @RequestMapping({"/","/v1/home"})
+@RequestMapping("/v1")
+public class HomeCon{
+    @GetMapping( "/home")
     public String doHome(){
+        return "/v1/home/home";
+    }
+    @GetMapping({  "/home2"})
+    public String doHomeGet(){
+        return "/v1/home/home";
+    }
+    @PostMapping("/home2")
+    public String doHomePost(){
         return "/v1/home/home";
     }
 }
