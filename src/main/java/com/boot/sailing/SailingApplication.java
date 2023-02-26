d39c7ee04231efca303062893c1fc73e7628c588
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-02-26 15:00:32 +0900

    Spring Boot Basic Setting

--- src/main/java/com/boot/sailing/SailingApplication.java
+++ src/main/java/com/boot/sailing/SailingApplication.java
@@ -1,13 +1,14 @@
-package com.example.hello;
+package com.boot.sailing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloApplication {
+public class SailingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloApplication.class, args);
+        SpringApplication.run(SailingApplication.class, args);
+        System.out.println("Hello spring boot !!");
     }
 
 }
--- src/test/java/com/boot/sailing/SailingApplicationTests.java
+++ src/test/java/com/boot/sailing/SailingApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.hello;
+package com.boot.sailing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloApplicationTests {
+class SailingApplicationTests {
 
     @Test
     void contextLoads() {
