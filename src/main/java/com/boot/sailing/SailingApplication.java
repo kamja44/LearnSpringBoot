2ed5420de5ab7ebf42b68dea45807060cfe9da8d
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-02-26 15:09:23 +0900

    static 파일 셋팅

--- src/main/java/com/boot/sailing/SailingApplication.java
+++ src/main/java/com/boot/sailing/SailingApplication.java
@@ -8,7 +8,7 @@ public class SailingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SailingApplication.class, args);
-        System.out.println("Hello spring boot !!");
+
     }
 
 }
