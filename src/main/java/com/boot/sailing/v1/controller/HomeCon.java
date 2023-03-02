b13f38da525c95fd9c91648698e2c1b077baed5d
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-03-03 00:03:39 +0900

    @RestController

--- src/main/java/com/boot/sailing/v1/controller/HomeCon.java
+++ src/main/java/com/boot/sailing/v1/controller/HomeCon.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/v1")
@@ -19,5 +20,12 @@ public class HomeCon{
     @PostMapping("/home2")
     public String doHomePost(){
         return "/v1/home/home";
+    }
+
+    @GetMapping("/rest2")
+    @ResponseBody
+    public String doRest2(){
+        String strHtml = "<html><body> Hi Rest , Controller + ResponseBody <hr> Hi. </body></html>";
+        return strHtml;
     }
 }
---
+++ src/main/java/com/boot/sailing/v1/controller/RestHome.java
@@ -0,0 +1,14 @@
+package com.boot.sailing.v1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestHome {
+    @GetMapping("v1/rest")
+    public String doRest(){
+        String strHtml = "Hello Rest Controller";
+
+        return strHtml;
+    }
+}
